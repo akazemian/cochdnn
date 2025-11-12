7498238cbe09ecb3a3c722b65d49a5dbd5f5c2fe
akazemian <atlaskaz@stanford.edu>
2025-11-12 14:10:48 -0800

    .

--- setup.py
+++ setup.py
@@ -1,33 +1,11 @@
-#!/usr/bin/env python
-
-from setuptools import setup
-
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-requirements = [
-        "torch",
-        "torchaudio",
-        "h5py",
-        "numpy",
-        "dill",
-        "cox",
-        "tables",
-        "tqdm",
-        "resampy",
-        "tensorboardX",
-        "chcochleagram @ git+https://github.com/jenellefeather/chcochleagram.git"
-]
+from setuptools import setup, find_packages
 
 setup(
     name='cochdnn',
-    version='1.0.0',
-    description="Models trained on word, speaker, noise task with cochleagram front ends.",
-    long_description=readme,
-    author="Jenelle Feather",
-    author_email='jfeather@mit.edu',
-    install_requires=requirements,
-    py_modules=['tests','robustness','default_paths', 'analysis_scripts'],
-    license="MIT license",
-    keywords='audio, deep neural networks',
+    version='0.1',
+    packages=find_packages(),  # Automatically finds all packages in the directory
+    include_package_data=True,  # Include package data specified in MANIFEST.in
+    package_data={
+        '': ['*.cpp', '*.cu'],  # Include all .cpp files in the package
+    },
 )
